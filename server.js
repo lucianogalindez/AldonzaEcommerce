66033fa6ca5c446eab7a92c2da3145e0e0ac5384
luciano <luxtucu@gmail.com>
2020-12-07 18:43:36 -0300

    deploy aldonza

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({extended: true})) //estas dos lineas son importantes para que las peticiones post se hagan en el formato correcto
 
-app.use(express.static(path.join(__dirname, 'frontend/build')))
+/*app.use(express.static(path.join(__dirname, 'frontend/build')))*/
 
 mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/aldonza', {
     useNewUrlParser: true, 
