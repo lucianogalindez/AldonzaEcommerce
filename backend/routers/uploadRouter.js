5639bf62600c13aa1c196e700c1f1567bc0b9f1c
luciano <luxtucu@gmail.com>
2020-12-29 00:42:57 -0300

    delete products

--- backend/routers/uploadRouter.js
+++ backend/routers/uploadRouter.js
@@ -1,12 +1,16 @@
 import multer from 'multer'
 import express from 'express'
 import { isAuth } from '../utils.js';
+import path from 'path'
 
 const uploadRouter = express.Router();
+const __dirname = path.resolve();
+
+const path_file = path.join(__dirname, 'frontend', 'public', 'images')
 
 const storage = multer.diskStorage({
     destination (req, file, cb) {
-        cb(null, 'frontend/public/images')
+        cb(null, path_file)
     },
     filename (req, file, cb) {
         cb(null, `${Date.now()}.jpg`);
